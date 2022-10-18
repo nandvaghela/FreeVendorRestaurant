dc3d0f74c1db444a5c14f22d34f0040ae4edeeb5
nandvaghela <45141940+nandvaghela@users.noreply.github.com>
2022-10-18 14:59:12 -0400

    Vendor profile and validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -11,9 +12,31 @@ class UserForm(forms.ModelForm):
         fields = ['firstname', 'lastname', 'username', 'email', 'password']
 
     def clean(self):
-        cleaned_data = super(UserForm,self).clean()
+        cleaned_data = super(UserForm, self).clean()
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
 
         if password != confirm_password:
             raise forms.ValidationError("Password does not match.")
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'address', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),
+                                      validators=[allow_only_images_validator])
+    cover_photos = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),
+                                   validators=[allow_only_images_validator])
+
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photos', 'address', 'country', 'state', 'city',
+                  'pin_code', 'latitude', 'longitude']
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = "readonly"
--- vendor/views.py
+++ vendor/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
+from accounts.models import UserProfile
+from .models import Vendor
+from accounts.views import check_role_vendor
 
-# Create your views here.
-
+# Create your views here
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendor_profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
 
-    return render(request,'vendor/vendor_profile.html')
+    if request.method =="POST":
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        vendor_form = VendorForm(request.POST, request.FILES, instance=vendor)
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request,"Restaurant Profile Updated")
+            return redirect('vendor_profile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        vendor_form = VendorForm(instance=vendor)
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    return render(request, 'vendor/vendor_profile.html', context)
