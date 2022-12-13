92a0b8ff6bc6ed987d07fb84c3f107b243c045e6
nandvaghela <45141940+nandvaghela@users.noreply.github.com>
2022-12-12 20:52:44 -0500

    Deploy to Linode server

--- Free_Vendor_Restaurant/settings.py
+++ Free_Vendor_Restaurant/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.105.16.191', 'foodvendordjango.com', 'www.foodvendordjango.com', '127.0.0.1']
 
 # Application definition
 
